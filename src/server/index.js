6bb640ebd2fb1cfadd1cc36789fbba45ecd204c7
Elena Nedkova <lenuza@gmail.com>
2020-06-03 18:16:34 +0300

    added express, body-parser, cors and gitignore

---
+++ src/server/index.js
@@ -0,0 +1,21 @@
+const path = require('path')
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(express.static('dist'))
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/', function (req, res) {
+    res.sendFile('src/client/view/index.html')
+})
+
+// set port
+const port = process.env.port || 8000;
+const server = app.listen(port, () => {
+    console.log(`Hello, listening on port ${port}`);
+});
