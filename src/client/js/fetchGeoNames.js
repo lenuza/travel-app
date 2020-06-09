7b1e7248e940a45a9063db404a92be24a07ee294
Elena Nedkova <lenuza@gmail.com>
2020-06-09 19:02:30 +0300

    separated getting the input value and fetching geoNames in different functions

--- src/client/js/fetchGeoNames.js
+++ src/client/js/fetchGeoNames.js
@@ -9,11 +9,32 @@ function getCityData() {
     document.getElementById('trip-destination').value = ''
     const key = process.env.GEONAMES_APP_ID
 
+    displayData(city, key)
+}
+
+const fetchGeoName = (city, key) => {
     return fetch(url + city + '&maxRows=1&username=' + key)
-        .then(res => {
-            return res.text()
+            .then(res => {
+                return res.text()
+            })
+            .catch(err => {
+                console.log(err)
+            })
+}
+
+const buildData = (city, key) => {
+    return fetchGeoName(city, key)
+        .then(text => {
+            return JSON.parse(text)
         })
-        .catch(err => {
-            console.log(err)
+        .catch(console.log)
+}
+
+
+const displayData = (city, key) => {
+    buildData(city, key)
+        .then(output => {
+            console.log(output)
         })
+        .catch(console.log)
 }
